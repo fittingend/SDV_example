8ebdbca6c876c4493d62ad369239b8fe7a738947
Equester517 <junohb135@gmail.com>
2025-08-20 10:47:53 +0900

    fix: 심볼릭링크 스크립트 오타 수정 및 README 업데이트

--- src/eevp_main_machine/sub_symbollink.sh
+++ src/eevp_main_machine/sub_symbollink.sh
@@ -6,7 +6,7 @@ TARGET="adaptive_autosar"
 apps_sub="ap_katech ap_intelligent_wiper ap_lotte_service ap_service_creator ap_sesl_service ap_tsmart"
 
 # ap_app 안에 있는 앱 목록
-apps_ap="ap_ipcandler ap_sm ap_soa ap_soa_debugger ap_soa_dms ap_soa_driverseat ap_soa_hvac ap_soa_moodlamp ap_soa_power ap_soa_rearcurtain ap_soa_roa ap_soa_smartfilm ap_soa_wiper ap_subscriptionmanager"
+apps_ap="ap_ipchandler ap_sm ap_soa ap_soa_debugger ap_soa_dms ap_soa_driverseat ap_soa_hvac ap_soa_moodlamp ap_soa_power ap_soa_rearcurtain ap_soa_roa ap_soa_smartfilm ap_soa_wiper ap_subscriptionmanager"
 
 BASE_DIR_SUB="$(pwd)/subscription_app"
 BASE_DIR_AP="$(pwd)/ap_app"
