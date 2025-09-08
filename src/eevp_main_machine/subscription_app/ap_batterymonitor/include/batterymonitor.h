3f0f999f5b331dc93c3b8d352bc842ae6694df8b
Tsmart-Gaeun <rkdms56@tsmartall.com>
2025-09-08 11:31:51 +0900

    batterymonitor
    batterymonitor

---
+++ src/eevp_main_machine/subscription_app/ap_batterymonitor/include/batterymonitor.h
@@ -0,0 +1,89 @@
+#ifndef batterymonitor_h_
+#define batterymonitor_h_
+
+#include <csignal>
+#include <thread>
+#include <ara/log/logger.h>
+#include <condition_variable>
+#include <mutex>
+
+#include "eevp/control/soamlm_proxy.h"
+#include "eevp/subscription/service/subscriptionmanagement_proxy.h"
+
+// #include "IMlmListener.h"
+#include "ISubscriptionManagementListener.h"
+// #include "IBmsInfoListener.h"
+
+#include "MoodLampProxyImpl.h"
+#include "SubscriptionManagementProxyImpl.h"
+#include "BmsInfoProxyImpl.h"
+
+#include "batterymonitor_subfunction.h"
+
+namespace eevp {
+namespace control {
+
+class BATTERYMONITOR {
+public:
+    /// @brief Constructor
+    BATTERYMONITOR();
+    /// @brief Destructor
+    ~BATTERYMONITOR();
+
+    /// @brief Start S/W Component
+    bool Start();
+    /// @brief Run S/W Component
+    void Run();
+    /// @brief Terminate S/W Component
+    void Terminate();
+
+    /// Mood Lamp
+    void RequestMlmSetRgbColor(const std::uint8_t& colorTableIndex);
+    void RequestMlmSetBrightness(const std::uint16_t& brightness);
+
+    //Subscription Management
+    void notifySubscriptionInfo(const eevp::subscription::type::SubscriptionInfo& subscriptionInfo);
+    void getSubscriptionInfo();
+
+    /// @brief App name
+    static const eevp::type::String mAppname;
+
+
+private:
+    /// @brief Signal Handler
+    static void SignalHandler(std::int32_t signal);
+
+    /// @brief Find handler
+    void StartFindCallback(ara::com::ServiceHandleContainer<eevp::control::proxy::SoaMlmProxy::HandleType> services, ara::com::FindServiceHandle handle);
+
+    /// @brief set Running State
+    bool setRunningState();
+
+    /// @brief Find Control Proxy
+    bool startMlmProxy();
+    /// @brief Find MoodLamp Proxy
+    bool startBMSInfoProxy();
+    /// @brief Find BMSInfo Proxy
+    bool startSubscriptionManagementProxy();
+    /// @brief Flag of Running
+    static std::atomic_bool mRunning;
+    /// @brief Logger
+    ara::log::Logger& mLogger;
+    /// @brief Subscription Flag
+    bool mSubscription;
+
+    std::mutex mSubscriptionMutex;
+    std::condition_variable mSubscriptionCv;
+
+    std::shared_ptr<eevp::control::moodlamp::MoodLampProxyImpl> moodLampProxyImpl;
+    std::shared_ptr<eevp::bmsinfosrv::BmsInfoProxyImpl> bmsInfoProxyImpl;
+    std::shared_ptr<eevp::subscription::service::SubscriptionManagementProxyImpl> subscriptionManagementProxyImpl;
+
+    /// @brief Instance of Port {BATTERYMONITOR.RPortSubscriptionManagement}
+    //std::unique_ptr<BATTERYMONITOR::aa::port::RPortSubscriptionManagement> m_RPortSubscriptionManagement;
+};
+
+}   // namespace control
+}   // namespace eevp
+
+#endif 
---
+++ src/eevp_main_machine/subscription_app/ap_batterymonitor/src/BmsInfoProxyImpl.cpp
@@ -0,0 +1,162 @@
+#include "BmsInfoProxyImpl.h"
+
+using namespace ara::core;
+using namespace eevp;
+using namespace eevp::bmsinfosrv;
+// using namespace eevp::bmsinfosrv::service;
+
+namespace eevp {
+namespace bmsinfosrv {
+// namespace proxy {
+
+BmsInfoProxyImpl::BmsInfoProxyImpl() :
+        mProxy{nullptr},
+        mFindHandle{nullptr},
+        mHandle{},
+        cvHandle{},
+    mLogger(ara::log::CreateLogger("KATC", "SUBM", ara::log::LogLevel::kInfo)) {
+    mLogger.LogInfo() << __func__;
+}
+
+BmsInfoProxyImpl::~BmsInfoProxyImpl() {
+    if (mProxy) {
+        mProxy->StopFindService(*mFindHandle);
+        mProxy.reset();
+    }
+}
+
+// void
+// BmsInfoProxyImpl::setEventListener(std::shared_ptr<IBmsInfoListener> _listener) {
+//     listener = _listener;
+// }
+
+bool
+BmsInfoProxyImpl::init() {
+    mLogger.LogInfo() << __func__;
+
+    mLogger.LogInfo() << "BmsInfo StartFindService() Start!!";
+    // 어떤 서비스를 찾을지 지정 (InstanceSpecifier는 AUTOSAR ARA::COM에서 사용되는 서비스 식별자)
+    ara::core::InstanceSpecifier specifier("BatteryMonitor/AA/RPort_BmsInfo");
+
+    // 서비스가 발견되었을 때 호출되는 콜백 정의
+    auto callback = [&](auto container, auto findHandle) {
+        FindServiceCallback(container, findHandle);// 서비스 연결 시도
+    };
+    // FindService의 결과 기다릴 때 사용할 mutex lock
+    std::unique_lock<std::mutex> lock(mHandle);
+
+    // 비동기적으로 서비스 탐색 시작 (서비스 발견 시 callback 호출됨)
+    auto result = proxy::BmsInfoSrvProxy::StartFindService(callback, specifier);
+    
+    // 일정 시간 동안 서비스 탐색 결과 대기
+    if (cvHandle.wait_for(lock, std::chrono::milliseconds(1000)) == std::cv_status::timeout) {
+        return false;
+    }
+
+    //log
+    auto error = result.Error();
+    mLogger.LogError() << "Error details: ErrorCode = " << error;
+    //log
+    
+    // StartFindService 자체가 실패한 경우
+    if (!result.HasValue()) {
+        mLogger.LogInfo() << "BmsInfoSrvProxy StartFindService() Failed";
+        return false;
+    }
+
+    return true;// 초기화 성공
+}
+
+void
+BmsInfoProxyImpl::FindServiceCallback(
+        ara::com::ServiceHandleContainer<eevp::bmsinfosrv::proxy::BmsInfoSrvProxy::HandleType> container,
+        ara::com::FindServiceHandle findHandle) {
+    mLogger.LogInfo() << __func__;
+
+    // 서비스 핸들 보호용 mutex lock
+    std::lock_guard<std::mutex> lock(mHandle);
+    
+    // 이미 프록시가 존재한다면 기존 구독 해제 및 프록시 제거
+    if (mProxy != nullptr) {
+        UnsubscribeBmsInfo();// 기존 BmsInfoSrvProxy 이벤트 핸들러 해제
+        mFindHandle = nullptr;// 이전 핸들 제거
+        mProxy = nullptr;// 프록시 포인터 제거
+    }
+    // 새로 받은 container가 비어있으면 → 유효한 서비스 없음 → 초기화 실패 처리
+    if (container.empty()) {
+        mProxy = nullptr;
+        return;
+    }
+    // 새로 발견한 서비스 핸들 저장
+    mFindHandle = std::make_shared<ara::com::FindServiceHandle>(findHandle);
+    // 새로운 BmsInfoSrvProxy 생성
+    mProxy = std::make_shared<proxy::BmsInfoSrvProxy>(container.at(0));
+
+    // BmsInfoSrvProxy 수신 시작 (리스너 등록)
+    SubscribeBmsInfo();
+    // 대기 중인 init() 측 조건 변수 깨움
+    cvHandle.notify_one();
+}
+
+void
+BmsInfoProxyImpl::SubscribeBmsInfo() {
+    mLogger.LogInfo() << __func__;
+
+    if (mProxy == nullptr) {
+        return;
+    }
+
+    // 콜백 함수 등록 (이벤트 수신 시 cbBmsInfo 호출되도록 설정)
+    auto result = mProxy->ems_BmsInfo.SetReceiveHandler(std::bind(&BmsInfoProxyImpl::cbBmsInfo, this));
+    if (!result.HasValue()) {
+        mLogger.LogWarn() << "Failed to SetReceiveHandler for cbBmsInfo with " << result.Error().Message();
+    }
+
+    // 실제로 이벤트 구독 시작 (Queue 사이즈 10)
+    result = mProxy->ems_BmsInfo.Subscribe(10);
+    if (!result.HasValue()) {
+        mLogger.LogWarn() << "Failed to Subscribe for cbBmsInfo with " << result.Error().Message();
+    }
+}
+
+void
+BmsInfoProxyImpl::cbBmsInfo() {
+    mLogger.LogInfo() << __func__;
+
+    if (mProxy == nullptr) {
+        return;
+    }
+
+    mProxy->ems_BmsInfo.GetNewSamples([&](auto msg) {
+
+        if(subscription_status == 1)
+        {
+            const auto& bmsInfo = *msg;
+            memcpy(&batterymonitor_B.RxTriggered, &bmsInfo, sizeof(bmsInfo));
+            mLogger.LogInfo() << "Recieve Success";
+            mLogger.LogInfo() << "cbBmsInfo : DataSerialNumber = " << batterymonitor_B.RxTriggered.DataSerialNumber;
+
+            // 실제 동작 수행 - bmsinfo&msg&lamporder
+            BatteryMonitor_triggered_sys(batterymonitor_B, &brightness);
+        }
+
+        // // 외부에서 등록한 리스너가 있다면 전달 (예: KATECH::BmsInfoSrvProxy 호출됨)
+        // if (listener != nullptr) {
+        //     listener->BmsInfoSrvProxy(bmsInfo);
+        // }
+    });
+}
+void
+BmsInfoProxyImpl::UnsubscribeBmsInfo() {
+    mLogger.LogInfo() << __func__;
+    if (mProxy == nullptr) {
+        return;
+    }
+
+    mProxy->ems_BmsInfo.Unsubscribe();
+}
+
+
+// } /// namespace proxy
+} /// namespace bmsInfo
+} /// namespace eevp
