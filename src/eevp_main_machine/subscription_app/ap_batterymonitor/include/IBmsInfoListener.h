037c1f7c2e4a1bd435390e6779c57522a1c100f6
Tsmart <rkdms56@tsmartall.com>
2025-09-09 16:46:41 +0900

    Update IBmsInfoListener.h

--- src/eevp_main_machine/subscription_app/ap_batterymonitor/include/IBmsInfoListener.h
+++ src/eevp_main_machine/subscription_app/ap_batterymonitor/include/IBmsInfoListener.h
@@ -1,6 +1,6 @@
 #ifndef IBMS_INFO_LISTENER_H
 #define IBMS_INFO_LISTENER_H
-// #include "eevp/bmsinfo/impl_type_ems_bmsinfo.h"
+#include "eevp/bmsinfo/impl_type_struct_bmsinfo.h"
 
 namespace eevp {
 namespace bmsinfo {
@@ -9,7 +9,7 @@ namespace service {
 class IBmsInfoListener {
 public:
     virtual ~IBmsInfoListener() {};
-    // virtual void notifyBmsInfo(const eevp::subscription::type::SubscriptionInfo& subscriptionInfo) = 0;
+    virtual void ems_BmsInfo(const eevp::bmsinfo::Struct_BmsInfo& bmsInfo) = 0;
    
 
 };
